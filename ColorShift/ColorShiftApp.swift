4462065389eba330025adaa807d4ee09de6c9210
Brett Koster <60300571+SeekAndDefineBK@users.noreply.github.com>
2023-09-20 22:17:00 -0500

    Initial Commit

---
+++ ColorShift/ColorShiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorShiftApp.swift
+//  ColorShift
+//
+//  Created by Brett Koster on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorShiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorShift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorShift
+//
+//  Created by Brett Koster on 9/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
